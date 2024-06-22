f98899886394ed1afd1c778b091882f505a6a9b1
HaHoLuLa <jh610.lee@gmail.com>
2024-06-22 18:10:54 +0900

    ui 작업중

--- type/src/App.tsx
+++ type/src/App.tsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <>
       <header className="ui">
-        <div className="flex justify-between my-3">
+        <div className="flex justify-between my-4">
           <h1 className="text-4xl font-bold self-center">HaHoLuLa</h1>
           <p className="self-center">로그인</p>
         </div>
@@ -14,21 +14,38 @@ const App = () => {
         </div>
 
         <div>
-          {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item, index) => (
-            <div className="flex justify-between flex-wrap">
-              <span className="md:inline hidden">{index + 1}</span>
-              <span className="md:w-1/2 w-full">안녕하세요</span>
-              <span className="md:m-0 ml-auto">작성자</span>
-              <span>작성일</span>
-              <span>조회수</span>
+          <div className="flex justify-between flex-wrap md:px-7 first:border-t border-b">
+            <span className="md:inline hidden w-14 text-center">글번호</span>
+            <span className="md:w-2/3 w-full">제목</span>
+            <span className="md:m-0 mr-auto">작성자</span>
+            <span>작성일</span>
+            <span>조회수</span>
+          </div>
+          {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item, index) => (
+            <div key={index} className="flex justify-between flex-wrap md:px-10 first:border-t border-b">
+              <span className="md:inline hidden w-5 text-center">{index + 1}</span>
+              <span className="md:w-2/3 w-full">안녕하세요</span>
+              <span className="md:m-0 mr-auto">작성자</span>
+              <span>2024-06-22</span>
+              <span>0</span>
             </div>
           ))}
         </div>
+
+        <div className="flex justify-center">
+          <ul className="flex">
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+          </ul>
+        </div>
       </main>
 
-      <footer className="ui">
+      {/* <footer className="ui">
         푸터
-      </footer>
+      </footer> */}
     </>
   )
 }
