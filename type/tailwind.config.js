620d41a1a8b9364d8d3ca58946804fd25bc38f83
HaHoLuLa <jh610.lee@gmail.com>
2024-06-22 22:46:30 +0900

    config

--- type/tailwind.config.js
+++ type/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Tailwind CSS를 적용할 파일들
+    "./src/**/*.{js,jsx,ts,tsx,css,html}", // Tailwind CSS를 적용할 파일들
   ],
   theme: {
     extend: {},
